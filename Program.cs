c3e03aa1316fcf45428094a4613747fa828f9ca4
zacharienoletGit <zacharie.nolet007@gmail.com>
2025-10-04 18:06:21 -0400

    fini

--- Program.cs
+++ Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            JeuCollection jeux = new JeuCollection
+            {
+                new Jeu { Nom = "RS6", Cote = 8 },
+                new Jeu { Nom = "Skyrim", Cote = 9 },
+                new Jeu { Nom = "PubG", Cote = 7 },
+                new Jeu { Nom = "Roblox", Cote = -2 }
+            };
+
+            IJeuCoteIterator iterateurCote = jeux.GetIterator();
+
+            while (iterateurCote.MoveNext())
+            {
+                Console.WriteLine($"{iterateurCote.JeuCourant.Nom} - Cote: {iterateurCote.JeuCourant.Nom}");
+            }
         }
     }
 }
